17669ded617531f525444107551e37fe16ae9331
manuelrojas19 <manuelarr99@gmail.com>
2021-04-12 20:57:46 -0500

    Correct ProfessorRepository functionality

--- src/main/java/mx/ipn/upiicsa/web/classroom/repository/ProfessorRepository.java
+++ src/main/java/mx/ipn/upiicsa/web/classroom/repository/ProfessorRepository.java
@@ -3,17 +3,21 @@ package mx.ipn.upiicsa.web.classroom.repository;
 import mx.ipn.upiicsa.web.classroom.model.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ProfessorRepository extends JpaRepository<Professor, Long> {
     Optional<Professor> findProfesorById(Long id);
+
     void deleteProfesorById(Long id);
+
     // TODO Join with Student table
     @Query("" +
             "SELECT CASE WHEN COUNT(p) > 0 THEN " +
             "TRUE ELSE FALSE END " +
-            "FROM Profesor p " +
+            "FROM Professor p " +
             "WHERE p.email = ?1"
     )
     Boolean existsEmail(String email);
