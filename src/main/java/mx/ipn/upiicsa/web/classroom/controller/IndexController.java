0b361da97abc0b4dd3ed1a3e3045a04d6ed3f543
manuelrojas19 <manuelarr99@gmail.com>
2021-04-12 21:49:13 -0500

    Change IndexController

--- src/main/java/mx/ipn/upiicsa/web/classroom/controller/IndexController.java
+++ src/main/java/mx/ipn/upiicsa/web/classroom/controller/IndexController.java
@@ -3,14 +3,14 @@ package mx.ipn.upiicsa.web.classroom.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController implements ErrorController {
     private static final String ERROR_MAPPING = "/error";
 
-    @RequestMapping(value = ERROR_MAPPING)
+    @GetMapping(value = ERROR_MAPPING)
     public ResponseEntity<String> error() {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
